392b2f97cc0de46c101e619659361f6300f81fe7
Marcia <marcianitayteycris@gmail.com>
2022-09-30 18:07:33 -0400

    aula 29/09

---
+++ src/js/nav.js
@@ -0,0 +1,18 @@
+export const nav = /* html*/ `
+<nav class="container-fluid">
+<ul>
+  <li><a class="contrast" href=" ./index.html">Desenvolvimento Web 1</a></li>
+</ul>
+<ul>
+  <li>
+    <details role="list" dir="rtl">
+      <summary role="link" class="contrast">Exercícios</summary>
+      <ul role="listbox">
+        <li><a class="primary" href="./login.html">Formulário de Acesso</a></li>
+      </ul>
+    </details>
+  </li>
+</ul>
+</nav>
+
+`
